e63a70ddfcecb157e83e7144e47c2469f0a2ac67
Ivan Kudryavtsev <ivakudry@gmail.com>
2025-08-14 11:27:52 +0300

    create event

--- 18/internal/handlers/create.go
+++ 18/internal/handlers/create.go
@@ -1,1 +1,35 @@
 package handlers
+
+import (
+	"calendar/internal/models"
+	"encoding/json"
+	"io"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func CreateEvent(st Storage) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		body := c.Request().Body
+		defer body.Close()
+
+		data, err := io.ReadAll(body)
+		if err != nil {
+			return c.String(http.StatusBadRequest, err.Error())
+		}
+
+		var event models.Event
+		if err := json.Unmarshal(data, &event); err != nil {
+			return c.String(http.StatusBadRequest, err.Error())
+		}
+
+		if err := c.Validate(event); err != nil {
+			return c.String(http.StatusBadRequest, err.Error())
+		}
+
+		out := st.Create(event.UserID, event.Date, event.Text)
+
+		return c.JSON(http.StatusOK, out)
+	}
+}
